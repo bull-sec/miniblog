ebfa2fe952b3482d69e11b173ac1bcaa174ce792
PaintsPlastic <paintsplastic@gmail.com>
2026-07-19 11:09:59 +0000

    changed behaviour of build script

--- scripts/localbuild.sh
+++ scripts/localbuild.sh
@@ -5,31 +5,6 @@ set -e
 echo "Cleaning site..."
 rm -rf _site/*
 
-echo "Converting images..."
-
-find assets/images -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) | while read img; do
-
-    out="${img%.*}.webp"
-
-    if [ ! -f "$out" ]; then
-        echo "Converting $img"
-        cwebp -q 85 "$img" -o "$out"
-    fi
-
-done
-
-echo "Backing up originals..."
-
-mkdir -p _image-backup
-
-find assets/images -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) -exec cp --parents {} _image-backup/ \;
-
-
-echo "Removing originals..."
-
-find assets/images -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) -delete
-
-
 echo "Building Jekyll..."
 
 jekyll serve --watch --incremental
